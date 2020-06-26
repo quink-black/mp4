67bb8e67a2d24a34a5bd95cb1f3b208417be7a8a
Zhao Zhili <quinkblack@foxmail.com>
2020-06-26 15:57:24 +0800

    add clang-format

--- mp4.cpp
+++ mp4.cpp
@@ -1,33 +1,62 @@
 #include "mp4.h"
 
+#include <getopt.h>
 #include <stdlib.h>
 #include <iostream>
 
-void dumpBox(const mov::Box::Boxes &boxes, int depth = 0) {
+void dumpBox(const mov::Box::Boxes &boxes, bool verbose, int depth = 0) {
     for (const auto &box : boxes) {
-        std::cout << std::string(depth, '\t')
+        std::cout << std::string(depth * 4, ' ')
                   << "type " << box->boxTypeStr()
                   << ", offset " << box->offset()
                   << ", size " << box->size();
-        bool skip_ctts = true;
-        if (skip_ctts && box->boxTypeStr() == "ctts") {
-            std::cout << '\n';
-            continue;
+        auto detail = box->detail();
+        if (verbose) {
+            std::cout << ", " << box->detail() << '\n';
+        } else {
+            auto second_newline = detail.find('\n', detail.find('\n') + 1);
+            if (second_newline != std::string::npos) {
+                std::cout << ", " << detail.substr(0, second_newline)
+                          << " ...\n";
+            } else {
+                std::cout << detail << '\n';
+            }
         }
-        std::cout << ", " << box->detail() << '\n';
         if (box->hasChild()) {
-            dumpBox(box->children(), depth + 1);
+            dumpBox(box->children(), verbose, depth + 1);
         }
     }
 }
 
+static void usage(const char *arg0) {
+    std::cout << "usage: " << arg0 << " -v file.mp4\n";
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        std::cout << "usage: " << argv[0] << " file.mp4\n";
+        usage(argv[0]);
         return EXIT_FAILURE;
     }
-    auto boxes = mov::Mp4Paser::parse(argv[1]);
-    dumpBox(boxes);
+
+    int ch = 0;
+    bool verbose = false;
+
+    while ((ch = getopt(argc, argv, "v")) != -1) {
+        switch (ch) {
+            case 'v':
+                verbose = true;
+                break;
+            case '?':
+            default:
+                usage(argv[0]);
+                return EXIT_SUCCESS;
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
+    auto boxes = mov::Mp4Paser::parse(*argv);
+    dumpBox(boxes, verbose);
 
     return EXIT_SUCCESS;
 }
